0c108ae7dc515805af8cf5f2f0ea206b2c1e1e99
Mehmet obi <mehmetobi01@gmail.com>
2022-10-05 23:33:33 +0300

    Chapter06 Exercises 01 and 02 done

---
+++ src/main/java/Chapters/Chapter06/Exercises06/Exercises06_Soru03.java
@@ -0,0 +1,37 @@
+package Chapters.Chapter06.Exercises06;
+
+import java.util.Scanner;
+
+public class Exercises06_Soru03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an integer to learn is it palindrom :");
+        int integer = input.nextInt();
+        if (isPalindrom(integer)) {
+            System.out.println("The reverse of " + integer + " is " + reverse(integer) + " Yes it is a palindrom");
+        } else {
+            System.out.println("The reverse of " + integer + " is " + reverse(integer) + " No it is not a palindrom");
+        }
+    }
+
+
+    public static int reverse(int number) {
+        String s = number + "";
+        int length = s.length();
+        String reverseS = "";
+        for (int i = length - 1; i >= 0; i--) {
+            reverseS += s.charAt(i);
+        }
+        return Integer.parseInt(reverseS);
+    }
+
+    public static boolean isPalindrom(int number) {
+        boolean isPalindrom;
+        if (number == reverse(number)) {
+            isPalindrom = true;
+        } else {
+            isPalindrom = false;
+        }
+        return isPalindrom;
+    }
+}
