4332f1fd44a1f9c79b71815d9b5c89285a5fe1e6
Mehmet obi <mehmetobi01@gmail.com>
2022-10-06 13:00:39 +0300

    Chapter06 Exercise 06 done

--- src/main/java/Chapters/Chapter06/Exercises06/Exercises06_Soru06.java
+++ src/main/java/Chapters/Chapter06/Exercises06/Exercises06_Soru06.java
@@ -2,20 +2,19 @@ package Chapters.Chapter06.Exercises06;
 
 public class Exercises06_Soru06 {
     public static void main(String[] args) {
-        displayPattern(7); //Test
+displayPattern(7);
     }
 
     public static void displayPattern(int n) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = n; j >= 0; j--) {
-                System.out.print("   ");
-            }
-            for (int k = i; k > 0; k--) {
-                System.out.print(k + "  ");
-            }
-            System.out.println();
+    for(int i=1; i<= n; i++) {
+        for(int j = n- i; j>0; j--){
+            System.out.print("  ");
         }
-
+        for(int k=i; k>0; k--){
+            System.out.print(k+" ");
+        }
+        System.out.println();
+    }
 
     }
 }
