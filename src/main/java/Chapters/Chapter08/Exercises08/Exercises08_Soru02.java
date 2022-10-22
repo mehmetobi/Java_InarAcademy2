f2d8ca62a30d44499d31e19cc62f04a694ec8261
Mehmet obi <mehmetobi01@gmail.com>
2022-10-22 12:05:45 +0300

    Chapter08 .exercises 01-02 added.

--- src/main/java/Chapters/Chapter08/Exercises08/Exercises08_Soru02.java
+++ src/main/java/Chapters/Chapter08/Exercises08/Exercises08_Soru02.java
@@ -1,4 +1,26 @@
 package Chapters.Chapter08.Exercises08;
 
+import java.util.Scanner;
+
 public class Exercises08_Soru02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a 4-by-4 matrix row by row:");
+        double[][] numbers = new double[4][4];
+        for (int row = 0; row < numbers.length; row++) {
+            for (int column = 0; column < numbers[row].length; column++) {
+                numbers[row][column] = input.nextDouble();
+            }
+        }
+        System.out.println("Sum of the elements in the major diagonal is " + sumMajorDiagonal(numbers));
+}
+    public static double sumMajorDiagonal(double[][] numbers){
+        double sum = 0;
+        for (int row = 0; row < numbers.length; row++) {
+            for (int column = row; column == row; column++) {
+                sum += numbers[row][column];
+            }
+        }
+        return sum;
+    }
 }
