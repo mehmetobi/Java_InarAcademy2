ad02edf8e76c514443b08090bf017d2ab1c0a359
Mehmet obi <mehmetobi01@gmail.com>
2022-11-05 16:25:06 +0300

    Chapter09 Exercise 05 added

---
+++ src/main/java/Chapters/Chapter09/Exercises09/Exercises09_Soru05/Exercises09_Soru05.java
@@ -0,0 +1,21 @@
+package Chapters.Chapter09.Exercises09.Exercises09_Soru05;
+
+import java.util.GregorianCalendar;
+
+public class Exercises09_Soru05 {
+    public static void main(String[] args) {
+        GregorianCalendar calendar = new GregorianCalendar();
+        int year = calendar.get(GregorianCalendar.YEAR);
+        int month = calendar.get(GregorianCalendar.MONTH);
+        int day = calendar.get(GregorianCalendar.DAY_OF_MONTH);
+        System.out.println("year: " + year + "\t\tmonth: " + month + "\t\tday: " + day);
+
+        calendar.setTimeInMillis(1234567898765L);
+
+        year = calendar.get(GregorianCalendar.YEAR);
+        month = calendar.get(GregorianCalendar.MONTH);
+        day = calendar.get(GregorianCalendar.DAY_OF_MONTH);
+        System.out.println("year: " + year + "\t\tmonth: " + month + "\t\tday: " + day);
+
+    }
+}
